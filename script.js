9856a8e17790bc3de3af6671a98ec293f0f59a41
Justin <jwine85@xombox.net>
2013-11-29 02:07:01 -0500

    adjustin canvas size.

--- script.js
+++ script.js
@@ -10,23 +10,28 @@ canvas.Scene.new({
   name: "testScene", //name of the scene
   materials: {
     images: {
-      img_id: "examples/tiled/images/tiles_spritesheet.png"
+      tileset: "examples/tiled/images/tiles_spritesheet.png"
     }
   },
-  preload: function(stage, pourcent) {
+  preload: function(stage, percent) {
 
   },
   ready: function(stage){
-    this.element = this.createElement();
-    this.element.drawImage("img_id");
-    stage.append(this.element);
-    
-    canvas.Input.keyDown(Input.A, function(e){
+    var el = this.createElement();
+    var tiled = canvas.Tiled.new();
+    tiled.ready(function(){
+      var tile_w = this.getTileWidth(),
+          tile_h = this.getTileHeight(),
+	  layer_object = this.getLayerObject();
+      stage.append(el);
+    });
+    tiled.load(this,el, "map/map.json");
+
+    canvas.Input.keyDown(Input.A, function(e){ //input testing, Picks up A press
       console.log("A is pressed");
     });
   },
   render: function(stage){
-    this.element.x +=1;
     stage.refresh();
   },
   exit: function(stage){
